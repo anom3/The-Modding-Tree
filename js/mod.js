9bb19f12e0b78c82c83936c0f50fadd1670fb014
anom3 <97539748+anom3@users.noreply.github.com>
2022-08-27 11:53:47 +0100

    OK

--- js/mod.js
+++ js/mod.js
@@ -56,7 +56,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("e1e11"))
 }
 
 
